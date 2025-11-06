5eca3d1b0052fe81a5d23cfec1774ff81cd19549
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-07 00:00:26 +0530

    Finally setting grids

--- grid.lua
+++ grid.lua
@@ -3,16 +3,29 @@ local grid = {
 }
 
 function grid:create()
-    self.x = 0
-    self.y = 0
-    
-    self.countX = 50
-    self.countY = 50
 
-    self.size = 48
+    local size = 48
+    self.x = size
+    self.y = size
+    
+    self.countX = 10
+    self.countY = 10
+
+    self.size = size
     
     self.width = self.size * self.countX
     self.height = self.size * self.countY
+
+end
+
+function grid:draw()
+    love.graphics.setLineWidth(0.5)
+    
+    for i = 0, self.countX do
+        for j = 0, self.countY do
+            love.graphics.rectangle("line", self.x + i * self.size, self.y + j * self.size, self.size, self.size )     
+        end
+    end
 
 end
 
--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
+wW, wH = love.graphics.getDimensions()
+
 local grid = require "grid"
+
+
+function love.load()
+    grid:create()
+end
+
+function love.draw()
+    grid:draw()
+end
