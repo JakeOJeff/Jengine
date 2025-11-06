b0f44f18c1421abc480f0efd538af1c693464b9b
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-06 23:52:55 +0530

    Setting default variables

--- grid.lua
+++ grid.lua
@@ -4,9 +4,15 @@ local grid = {
 
 function grid:create()
     self.x = 0
-    self.y
+    self.y = 0
+    
+    self.countX = 50
+    self.countY = 50
 
     self.size = 48
+    
+    self.width = self.size * self.countX
+    self.height = self.size * self.countY
 
 end
 
