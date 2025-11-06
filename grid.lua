1e67094ee4ac6918d9e6a9159455c1591857da88
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-07 00:30:08 +0530

    Fixing the update sequence

--- grid.lua
+++ grid.lua
@@ -1,16 +1,12 @@
-local grid = {
-
-}
+local grid = {}
 
 function grid:create()
-
     local size = 48
     self.x = size
     self.y = size
     
     self.countX = 10
     self.countY = 10
-
     self.size = size
     
     self.width = self.size * self.countX
@@ -18,32 +14,51 @@ function grid:create()
     
     self.grids = {}
 
-    for i = 1, #self.grids do
+    for i = 1, self.countX do
         self.grids[i] = {}
-        for j = 1, #self.grids do
+        for j = 1, self.countY do
             self.grids[i][j] = {
-                x = self.x + ((i-1) * self.size),
-                y = self.y + ((j-1) * self.size),
+                x = self.x + ((i - 1) * self.size),
+                y = self.y + ((j - 1) * self.size),
                 hovering = false,
                 occupied = false
             }
         end
     end
+end
 
+function grid:update()
+    local mx, my = love.mouse.getPosition()
+
+    for i = 1, self.countX do
+        for j = 1, self.countY do
+            local cell = self.grids[i][j]
+            if mx > cell.x and mx < cell.x + self.size and
+               my > cell.y and my < cell.y + self.size then
+                cell.hovering = true
+            else
+                cell.hovering = false
+            end
+        end
+    end
 end
 
 function grid:draw()
     love.graphics.setLineWidth(0.5)
     
-    for i = 1, #self.grids do
-        for j = 1, #self.grids[i] do
-            if self.grids[i][j].hovering then
+    for i = 1, self.countX do
+        for j = 1, self.countY do
+            local cell = self.grids[i][j]
+
+            love.graphics.rectangle("line", cell.x, cell.y, self.size, self.size)
+            if cell.hovering then
                 love.graphics.setColor(0.7, 0.7, 1)
+               love.graphics.rectangle("fill", cell.x, cell.y, self.size, self.size) 
+            else
+                love.graphics.setColor(1, 1, 1)
             end
-            love.graphics.rectangle("line", self.grids[i][j].x, self.grids[i][j].y, self.size, self.size )     
         end
     end
-
 end
 
 return grid
--- main.lua
+++ main.lua
@@ -6,7 +6,9 @@ local grid = require "grid"
 function love.load()
     grid:create()
 end
-
+function love.update()
+    grid:update()
+end
 function love.draw()
     grid:draw()
 end
