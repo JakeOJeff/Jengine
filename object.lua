890423d2a9c5669d7d30c3a3d8da0b3b8dcdb99e
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-07 12:48:31 +0530

    Adding Object

---
+++ object.lua
@@ -0,0 +1,21 @@
+local object = {}
+object.__index = object
+
+function object:newRect(world, x, y, width, height, type, angle)
+    local self = setmetatable({}, object)
+    self.x = x + width / 2
+    self.y = y + height / 2
+    self.width = width
+    self.height = height
+    self.angle = angle or 0
+
+    self.body = love.physics.newBody(world, self.x, self.y, self.type)
+    self.shape = love.physics.newRectangleShape(self.width, self.height)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+    
+    self.body:setAngle(self.angle)
+
+    return self
+end
+
+return object
