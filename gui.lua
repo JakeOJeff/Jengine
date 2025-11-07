3444113d475f71054588bc06e34f99dd3c1b4598
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-07 17:56:22 +0530

    Creating a Draw function

--- gui.lua
+++ gui.lua
@@ -2,11 +2,39 @@ local gui = {}
 
 
 function gui:create()
-    local size = 48
+
+    self.size = 48
     
     self.countX = 2
     self.countY = 3
 
-    self.x = size * 12
-    self.y = grid.height
+    self.x = self.size * 12
+    self.y = Grid.height - (self.size * self.countY )
+
+    self.width = self.size * self.countX
+    self.height = self.size * self.countY
+
+    self.buttons = {}
+
+    for i = 1, self.countX do
+        self.buttons[i] = {}
+
+        for j = 1, self.countY do
+            self.buttons[i][j] = {
+                x = self.x + ((i - 1) * self.size),
+                y = self.y + ((j - 1) * self.size),
+                hovering = false,
+                seleted = false,
+                func = function ()
+                    
+                end,
+                draw = function ()
+                    
+                end
+            }
+        end
+    end
+
 end
+
+return gui
