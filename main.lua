74b1f60efe2c1df3243435aab7b3aa0b4afdb043
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-09 03:36:22 +0530

    Adding rotation and rotation speed

--- main.lua
+++ main.lua
@@ -8,26 +8,34 @@ Gui = require "gui"
 
 gravity = false
 rotation = 0
+targetRotation = 0
+rotationSpeed = math.rad(90)
 function love.load()
     Grid:create()
     Gui:create()
 end
+
 function love.update(dt)
     World:update(dt)
     Grid:update()
     Gui:update()
-            if gravity then
-            World:setGravity(0, 9.81 * 64)
-            print("Gravity ON")
-        else
-            World:setGravity(0, 0)
-            print("Gravity OFF")
-            for _, body in pairs(World:getBodies()) do
-                body:setLinearVelocity(0, 0)
-                body:setAngularVelocity(0)
-            end
+    if gravity then
+        World:setGravity(0, 9.81 * 64)
+        print("Gravity ON")
+    else
+        World:setGravity(0, 0)
+        print("Gravity OFF")
+        for _, body in pairs(World:getBodies()) do
+            body:setLinearVelocity(0, 0)
+            body:setAngularVelocity(0)
         end
+    end
+
+    if rotation < targetRotation then
+        rotation = rotation + rotationSpeed * dt
+    end
 end
+
 function love.draw()
     Grid:draw()
     Gui:draw()
@@ -36,16 +44,16 @@ end
 function love.keypressed(key, scancode, isrepeat)
     if key == "space" then
         gravity = not gravity
-
     elseif key == "r" then
         for _, body in pairs(World:getBodies()) do
             body:destroy()
         end
         Grid:create()
     elseif key == "e" then
-        rotation = rotation + math.rad(90)
+        targetRotation = targetRotation + math.rad(90)
     end
-end 
+end
+
 function love.mousepressed(x, y, button)
     if button == 1 then
         Gui:mousepressed(x, y)
